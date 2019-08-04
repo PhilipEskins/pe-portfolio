1a518514cb6e059844413fdf27981344362c912a
Philip Eskins <philipeskins@gmail.com>
2019-08-04 14:30:03 -0700

    start framework for navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import '../styles/navbar.scss';
+import philip from '../assets/img/Phil_Squared_Photo.jpg'
 
 function navbar() {
   return (
-    <div>
-      <h1>Navbar works</h1>
+    <div id ="navbar">
+      <h1>Navbar</h1>
+      <img id="mainPhoto" src={philip} alt="Philip" />
+      <ul>
+        <li>Projects</li>
+        <li>About</li>
+        <li>Resume</li>
+        <li>Contact</li>
+      </ul>
     </div>
   )
 };
