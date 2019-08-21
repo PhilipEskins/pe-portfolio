025756c51e14bf30f73afa3b875f114c44ff101b
Philip Eskins <philipeskins@gmail.com>
2019-08-20 18:39:53 -0700

    make fields in contact form required

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -10,15 +10,15 @@ function contact() {
         <input type="hidden" name="form-name" value="contact" />
         <div className="name">
           <label htmlFor="name">*Name</label>
-          <input type="text" id="name" name="name"/>
+          <input type="text" id="name" name="name" required/>
         </div>
         <div className="email">
           <label htmlFor="email">*Email</label>
-          <input type="email" id="email" name="email"/>
+          <input type="email" id="email" name="email" required/>
         </div>
         <div className="message">
           <label htmlFor="message">*Message</label>
-          <textarea id="message" placeholder="Type something here" name="message"></textarea>
+          <textarea id="message" placeholder="Type something here" name="message" required></textarea>
         </div>
         <div className="submit">
           <button type="submit">Submit</button>
